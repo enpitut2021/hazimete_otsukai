d48d4cae448ff1e311cfd5e0b7086322dfce506e
Taishi Ito <itohlee14@gmail.com>
2021-07-30 12:05:28 +0900

    タブに画面遷移機能をもたせる

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,12 @@ import 'package:flutter/material.dart';
 
 // ChangeNotifierProviderとConsumerウィジェットが使えるようになる
 import 'package:provider/provider.dart';
+import 'package:tobuy/tobuy_page.dart';
 
 // main_model.dartで定義したクラス有効化
 import 'main_model.dart';
+
+import 'tobuy_page.dart';
 
 void main() async {
   // この下の2行がないとFirebase使えないようになったらしい
@@ -42,12 +45,8 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final TextEditingController _fieldText = TextEditingController(text: null);
-
   @override
   Widget build(BuildContext context) {
-    final _focusNode = FocusNode();
-
     // ChangeNotifierBuilder: なんかstatelessでもstatefulにしてくれるやつ
     return ChangeNotifierProvider(
       // 下のやつで、MyHomePageが回ったタイミングでgetItemListRealtimeが実行される
@@ -67,117 +66,128 @@ class MyHomePage extends StatelessWidget {
                   .toList(),
             ),
           ),
-          body:
-              // Consumerの下ならmodelファイルを参照できるらしい
-              // (この場合main_model.dartのMainModelクラス)
-              Column(children: <Widget>[
-            // 入力フォーム
-            // なんかインデントおかしいね 自動整型なのに～
-            Consumer<MainModel>(builder: (context, model, child) {
-              return TextField(
-                controller: _fieldText,
-                decoration: InputDecoration(
-                  labelText: "　在庫を追加", // ラベル
-                  hintText: "　例) 醤油", // ヒント
-                ),
-                onChanged: (text) {
-                  // テキストフォームに入力されたテキスト(text)を
-                  // main_model.dartのMainModelクラス内で予め用意しといた
-                  // 空の文字列だったitemTextに挿入
-                  model.itemText = text;
-                },
-                focusNode: _focusNode, // フォーカス用の値
-                // Enterキーを押すことでアイテムを登録できる
-                onSubmitted: (value) async {
-                  try {
-                    await model.addItem();
-                    // テキストフォームのテキストを消す
-                    _fieldText.clear();
-                    // バグ: 内部のmodel.itemTextも消せば解消！
-                    model.itemText = '';
-                    _focusNode.requestFocus(); // フォーカスを再び渡す
-                  } catch (e) {
-                    await _showDialog(context, e.toString());
-                  }
-                },
-              );
-            }),
-            // 登録ボタン
-            // Consumer<MainModel>(builder: (context, model, child) {
-            //   // ElevatedButtonが旧RaisedButton
-            //   // ちなみにTextButtonは旧FlatButton
-            //   return ElevatedButton(
-            //     child: const Text("とうろく"),
-            //     style: ElevatedButton.styleFrom(
-            //         primary: Colors.red, onPrimary: Colors.grey[200]),
-            //     onPressed: () async {
-            //       // タップされたときの動作
-            //       // モデルを参照する
-            //       // (model.addItem → main_model.dartのMainModelクラスのaddItem()関数)
-            //       // Firestoreにデータを追加する
-            //       try {
-            //         await model.addItem();
-            //         _fieldText.clear();
-            //         model.itemText = '';
-            //       } catch (e) {
-            //         await _showDialog(context, e.toString());
-            //       }
-            //     },
-            //   );
-            // }),
-            SizedBox(
-              height: 10,
-            ),
-            // Divider(),
-            // リスト表示をつくるとこ
-            Consumer<MainModel>(builder: (context, model, child) {
-              final itemList = model.itemList;
-              // mapメソッドでitemList内の各itemをListTileに変換
-              // → .toList()で更にリストに変換
-              // → 変換結果をlistTiles変数に代入
-              final listTiles = itemList
-                  // for item in itemList ... みたいな感じ
-                  // itemList内の各itemを
-                  // ListTile(title: Text(item.title!))
-                  // の形式に割り当てていく
-                  .map((item) => ListTile(
-                        title: Text(item.title!),
-                      ))
-                  // 完成した複数のListTile(...)を.toList()でリストに変換する
-                  // (ListTileを束ねる)
-                  // ListView(children: ★) ← ★はリストになる必要があるから
-                  .toList();
-              // リストに変換されたlistTilesをListViewで描画
-              return Flexible(
-                  // ListView.separated: リストの各アイテムの間に線があるだけ
-                  child: ListView.separated(
-                      separatorBuilder: (context, index) => Divider(),
-                      itemCount: itemList.length,
-                      itemBuilder: (context, index) {
-                        if (index == itemList.length) {
-                          return const Divider(
-                            height: 1,
-                          );
-                        }
-                        return Dismissible(
-                          // childとしてListTileを複数取る
-                          // child: ListTile(), ListTile(), ...
-                          // のでlistTiles(ListTileのリスト)を[index]でループ
-                          child: listTiles[index],
-                          // これはなんかしらん
-                          key: UniqueKey(),
-                          background: Container(
-                              color: Colors.redAccent,
-                              child: Icon(Icons.clear, color: Colors.white)),
-                          onDismissed: (direction) {
-                            // 以下に消す処理を作成
-                            deleteItem(itemList[index]);
-                          },
-                        );
-                      }));
-            }),
-          ])),
+          body: TabBarView(
+            children: [StoragePage(), ToBuyPage()],
+          )
+          // Consumerの下ならmodelファイルを参照できるらしい
+          // (この場合main_model.dartのMainModelクラス)
+          ),
     );
+  }
+}
+
+class StoragePage extends StatelessWidget {
+  final TextEditingController _fieldText = TextEditingController(text: null);
+  final _focusNode = FocusNode();
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      // 入力フォーム
+      // なんかインデントおかしいね 自動整型なのに～
+      Consumer<MainModel>(builder: (context, model, child) {
+        return TextField(
+          controller: _fieldText,
+          decoration: InputDecoration(
+            labelText: "　在庫を追加", // ラベル
+            hintText: "　例) 醤油", // ヒント
+          ),
+          onChanged: (text) {
+            // テキストフォームに入力されたテキスト(text)を
+            // main_model.dartのMainModelクラス内で予め用意しといた
+            // 空の文字列だったitemTextに挿入
+            model.itemText = text;
+          },
+          focusNode: _focusNode, // フォーカス用の値
+          // Enterキーを押すことでアイテムを登録できる
+          onSubmitted: (value) async {
+            try {
+              await model.addItem();
+              // テキストフォームのテキストを消す
+              _fieldText.clear();
+              // バグ: 内部のmodel.itemTextも消せば解消！
+              model.itemText = '';
+              _focusNode.requestFocus(); // フォーカスを再び渡す
+            } catch (e) {
+              await _showDialog(context, e.toString());
+            }
+          },
+        );
+      }),
+      // 登録ボタン
+      // Consumer<MainModel>(builder: (context, model, child) {
+      //   // ElevatedButtonが旧RaisedButton
+      //   // ちなみにTextButtonは旧FlatButton
+      //   return ElevatedButton(
+      //     child: const Text("とうろく"),
+      //     style: ElevatedButton.styleFrom(
+      //         primary: Colors.red, onPrimary: Colors.grey[200]),
+      //     onPressed: () async {
+      //       // タップされたときの動作
+      //       // モデルを参照する
+      //       // (model.addItem → main_model.dartのMainModelクラスのaddItem()関数)
+      //       // Firestoreにデータを追加する
+      //       try {
+      //         await model.addItem();
+      //         _fieldText.clear();
+      //         model.itemText = '';
+      //       } catch (e) {
+      //         await _showDialog(context, e.toString());
+      //       }
+      //     },
+      //   );
+      // }),
+      SizedBox(
+        height: 10,
+      ),
+      // Divider(),
+      // リスト表示をつくるとこ
+      Consumer<MainModel>(builder: (context, model, child) {
+        final itemList = model.itemList;
+        // mapメソッドでitemList内の各itemをListTileに変換
+        // → .toList()で更にリストに変換
+        // → 変換結果をlistTiles変数に代入
+        final listTiles = itemList
+            // for item in itemList ... みたいな感じ
+            // itemList内の各itemを
+            // ListTile(title: Text(item.title!))
+            // の形式に割り当てていく
+            .map((item) => ListTile(
+                  title: Text(item.title!),
+                ))
+            // 完成した複数のListTile(...)を.toList()でリストに変換する
+            // (ListTileを束ねる)
+            // ListView(children: ★) ← ★はリストになる必要があるから
+            .toList();
+        // リストに変換されたlistTilesをListViewで描画
+        return Flexible(
+            // ListView.separated: リストの各アイテムの間に線があるだけ
+            child: ListView.separated(
+                separatorBuilder: (context, index) => Divider(),
+                itemCount: itemList.length,
+                itemBuilder: (context, index) {
+                  if (index == itemList.length) {
+                    return const Divider(
+                      height: 1,
+                    );
+                  }
+                  return Dismissible(
+                    // childとしてListTileを複数取る
+                    // child: ListTile(), ListTile(), ...
+                    // のでlistTiles(ListTileのリスト)を[index]でループ
+                    child: listTiles[index],
+                    // これはなんかしらん
+                    key: UniqueKey(),
+                    background: Container(
+                        color: Colors.redAccent,
+                        child: Icon(Icons.clear, color: Colors.white)),
+                    onDismissed: (direction) {
+                      // 以下に消す処理を作成
+                      deleteItem(itemList[index]);
+                    },
+                  );
+                }));
+      }),
+    ]);
   }
 
   Future _showDialog(BuildContext context, String title) async {
--- lib/tobuy_page.dart
+++ lib/tobuy_page.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:tobuy/tobuy_model.dart';
 
-class MyHomePage extends StatelessWidget {
+class ToBuyPage extends StatelessWidget {
   final TextEditingController _fieldText = TextEditingController(text: null);
 
   @override
@@ -11,80 +11,81 @@ class MyHomePage extends StatelessWidget {
     final _focusNode = FocusNode();
 
     return ChangeNotifierProvider(
-      create: (context) => ToBuyModel(),
-      child: Scaffold(
-          appBar: AppBar(
-            title: Text(
-              'TOBUY',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            centerTitle: true,
-            bottom: TabBar(
-              tabs: choices
-                  .map((choice) => Tab(
-                        text: choice.title,
-                      ))
-                  .toList(),
-            ),
+        create: (context) => ToBuyModel(),
+        child:
+            // Scaffold(
+            //     appBar: AppBar(
+            //       title: Text(
+            //         'TOBUY',
+            //         style: TextStyle(fontWeight: FontWeight.bold),
+            //       ),
+            //       centerTitle: true,
+            //       bottom: TabBar(
+            //         tabs: choices
+            //             .map((choice) => Tab(
+            //                   text: choice.title,
+            //                 ))
+            //             .toList(),
+            //       ),
+            //     ),
+            //     body:
+            Column(children: <Widget>[
+          Consumer<ToBuyModel>(builder: (context, model, child) {
+            return TextField(
+              controller: _fieldText,
+              decoration: InputDecoration(
+                labelText: "買うものを追加",
+                hintText: "　例) シャンプーの詰め替え",
+              ),
+              onChanged: (text) {
+                model.itemText = text;
+              },
+              focusNode: _focusNode,
+              onSubmitted: (value) async {
+                try {
+                  await model.addItem();
+                  _fieldText.clear();
+                  model.itemText = '';
+                  _focusNode.requestFocus();
+                } catch (e) {
+                  await _showDialog(context, e.toString());
+                }
+              },
+            );
+          }),
+          SizedBox(
+            height: 10,
           ),
-          body: Column(children: <Widget>[
-            Consumer<ToBuyModel>(builder: (context, model, child) {
-              return TextField(
-                controller: _fieldText,
-                decoration: InputDecoration(
-                  labelText: "買うものを追加",
-                  hintText: "　例) シャンプーの詰め替え",
-                ),
-                onChanged: (text) {
-                  model.itemText = text;
-                },
-                focusNode: _focusNode,
-                onSubmitted: (value) async {
-                  try {
-                    await model.addItem();
-                    _fieldText.clear();
-                    model.itemText = '';
-                    _focusNode.requestFocus();
-                  } catch (e) {
-                    await _showDialog(context, e.toString());
-                  }
-                },
-              );
-            }),
-            SizedBox(
-              height: 10,
-            ),
-            Consumer<ToBuyModel>(builder: (context, model, child) {
-              final itemList = model.itemList;
-              final listTiles = itemList
-                  .map((item) => ListTile(
-                        title: Text(item.title!),
-                      ))
-                  .toList();
-              return Flexible(
-                  child: ListView.separated(
-                      separatorBuilder: (context, index) => Divider(),
-                      itemCount: itemList.length,
-                      itemBuilder: (context, index) {
-                        if (index == itemList.length) {
-                          return const Divider(
-                            height: 1,
-                          );
-                        }
-                        return Dismissible(
-                          child: listTiles[index],
-                          key: UniqueKey(),
-                          background: Container(
-                              color: Colors.redAccent,
-                              child: Icon(Icons.clear, color: Colors.white)),
-                          onDismissed: (direction) {
-                            deleteItem(itemList[index]);
-                          },
+          Consumer<ToBuyModel>(builder: (context, model, child) {
+            final itemList = model.itemList;
+            final listTiles = itemList
+                .map((item) => ListTile(
+                      title: Text(item.title!),
+                    ))
+                .toList();
+            return Flexible(
+                child: ListView.separated(
+                    separatorBuilder: (context, index) => Divider(),
+                    itemCount: itemList.length,
+                    itemBuilder: (context, index) {
+                      if (index == itemList.length) {
+                        return const Divider(
+                          height: 1,
                         );
-                      }));
-            }),
-          ])),
-    );
+                      }
+                      return Dismissible(
+                        child: listTiles[index],
+                        key: UniqueKey(),
+                        background: Container(
+                            color: Colors.redAccent,
+                            child: Icon(Icons.clear, color: Colors.white)),
+                        onDismissed: (direction) {
+                          deleteItem(itemList[index]);
+                        },
+                      );
+                    }));
+          }),
+        ]));
   }
 
   Future _showDialog(BuildContext context, String title) async {
