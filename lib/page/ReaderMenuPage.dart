702cf9ade2213debbba5ca557b8ac7fb118fd321
Taishi Ito <itohlee14@gmail.com>
2021-08-03 11:14:37 +0900

    Cloud Vision APIによるテキスト抽出

--- lib/page/ReaderMenuPage.dart
+++ lib/page/ReaderMenuPage.dart
@@ -1,84 +1,167 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:tobuy/common/CameraController.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-import 'ReaderDetailPage.dart';
+import 'dart:convert';
+import 'package:cloud_functions/cloud_functions.dart';
 
 // -----------------------------------
 // クラス名　 : ReaderMenuPage
 // クラス概要 : 読み込みメニューページ
 // -----------------------------------
 class ReaderMenuPage extends StatefulWidget {
-  ReaderMenuPage({Key? key}) : super(key: key);
+  // null safety対応のため、Keyに?をつけ、titleは初期値""を設定
+  ReaderMenuPage({Key? key, this.title = ""}) : super(key: key);
+
+  final String title;
 
   @override
-  _ReaderMenuState createState() => _ReaderMenuState();
+  ReaderMenuState createState() => ReaderMenuState();
 }
 
-// -----------------------------------
-// クラス名　 : _ReaderMenuState
-// クラス概要 : 読み込みメニューページステート
-// -----------------------------------
-class _ReaderMenuState extends State<ReaderMenuPage> {
-  // 変数宣言
-  /* 読み込み方法 */ ImageSource? _imageSource = ImageSource.camera;
+class ReaderMenuState extends State<ReaderMenuPage> {
+  // null safety対応のため、?でnull許容
+  File? _image;
+
+  final _picker = ImagePicker();
+
+  // null safety対応のため、?でnull許容
+  String? _result;
+
+  @override
+  void initState() {
+    super.initState();
+    _signIn();
+  }
+
+  // 匿名でのFirebaseログイン処理
+  void _signIn() async {
+    await FirebaseAuth.instance.signInAnonymously();
+  }
+
+  Future _getImage(FileMode fileMode) async {
+    // null safety対応のため、lateで宣言
+    late final _pickedFile;
+
+    // image_pickerの機能で、カメラからとギャラリーからの2通りの画像取得（パスの取得）を設定
+    if (fileMode == FileMode.CAMERA) {
+      _pickedFile = await _picker.pickImage(source: ImageSource.camera);
+    } else {
+      _pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+    }
+
+    setState(() {
+      if (_pickedFile != null) {
+        _image = File(_pickedFile.path);
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("画像認識"),
+        title: Text('日本語OCR'),
       ),
-      body: Column(
-        children: <Widget>[
-          Text(
-            "読み込み方法を選択してください。",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
-            ),
-          ),
-          RadioListTile<ImageSource>(
-            title: Text("カメラ"),
-            groupValue: _imageSource,
-            value: ImageSource.camera,
-            onChanged: (value) {
-              _imageSource = value;
-              setState(() {});
-            },
-          ),
-          RadioListTile<ImageSource>(
-            title: Text("ギャラリー"),
-            groupValue: _imageSource,
-            value: ImageSource.gallery,
-            onChanged: (value) {
-              _imageSource = value;
-              setState(() {});
-            },
-          ),
-          ElevatedButton(
-              child: Text("スキャン開始"),
-              onPressed: () async {
-                // 指定された読み込み方法で画像を取得
-                try {
-                  File? file = await CameraController.getAndSaveImageFromDevice(
-                      _imageSource!);
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
 
-                  // 画像が取得できた場合に明細ページに遷移
-                  if (file == null) {
-                    throw Exception('ファイルを取得できませんでした');
-                  }
-                  Navigator.push(
-                    context,
-                    new MaterialPageRoute(
-                        builder: (context) => new ReaderDetailPage(file)),
-                  );
-                } catch (e) {}
-              }),
+          // 写真のサイズによって画面はみ出しエラーが生じるのを防ぐため、
+          // Columnの上にもSingleChildScrollViewをつける
+          child: SingleChildScrollView(
+            child: Column(children: [
+              // 画像を取得できたら表示
+              // null safety対応のため_image!とする（_imageはnullにならない）
+              if (_image != null) Image.file(_image!, height: 400),
+
+              // 画像を取得できたら解析ボタンを表示
+              if (_image != null) _analysisButton(),
+              Container(
+                  height: 240,
+
+                  // OCR（テキスト検索）の結果をスクロール表示できるようにするため
+                  // 結果表示部分をSingleChildScrollViewでラップ
+                  child: SingleChildScrollView(
+                      scrollDirection: Axis.vertical,
+                      child: Text((() {
+                        // OCR（テキスト認識）の結果（_result）を取得したら表示
+                        if (_result != null) {
+                          // null safety対応のため_result!とする（_resultはnullにならない）
+                          return _result!;
+                        } else if (_image != null) {
+                          return 'ボタンを押すと解析が始まります';
+                        } else {
+                          return 'OCR（テキスト認識）したい画像を撮影または読込んでください';
+                        }
+                      }())))),
+            ]),
+          ),
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          // カメラ起動ボタン
+          FloatingActionButton(
+            onPressed: () => _getImage(FileMode.CAMERA),
+            tooltip: 'Pick Image from camera',
+            child: Icon(Icons.camera_alt),
+          ),
+
+          // ギャラリー（ファイル）検索起動ボタン
+          FloatingActionButton(
+            onPressed: () => _getImage(FileMode.GALLERY),
+            tooltip: 'Pick Image from gallery',
+            child: Icon(Icons.folder_open),
+          ),
         ],
       ),
     );
   }
+
+  // OCR（テキスト認識）開始処理
+  Widget _analysisButton() {
+    return ElevatedButton(
+      child: Text('解析'),
+      onPressed: () async {
+        // null safety対応のため_image!とする（_imageはnullにならない）
+        List<int> _imageBytes = _image!.readAsBytesSync();
+        String _base64Image = base64Encode(_imageBytes);
+
+        // Firebase上にデプロイしたFunctionを呼び出す処理
+        HttpsCallable _callable =
+            FirebaseFunctions.instance.httpsCallable('annotateImage');
+        final params = '''{
+          "image": {"content": "$_base64Image"},
+          "features": [{"type": "TEXT_DETECTION"}],
+          "imageContext": {
+            "languageHints": ["ja"]
+          }
+        }''';
+
+        final _text = await _callable(params).then((v) {
+          return v.data[0]["fullTextAnnotation"]["text"];
+        }).catchError((e) {
+          print('ERROR: $e');
+          return '読み取りエラーです';
+        });
+
+        // OCR（テキスト認識）の結果を更新
+        setState(() {
+          _result = _text;
+        });
+      },
+    );
+  }
+}
+
+// カメラ経由かギャラリー（ファイル）経由かを示すフラグ
+enum FileMode {
+  CAMERA,
+  GALLERY,
 }
